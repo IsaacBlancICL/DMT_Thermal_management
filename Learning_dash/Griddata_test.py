25687646c0b93dade366a4ec52a363f6617fa0de
Isaac Blanc <ib3418@ic.ac.uk>
2022-02-10 12:43:37 +0000

    plotly volume working in griddata test
    Things I've learned:
    1. The larger the grid, the longer the html file takes to load in Chrome. Currently, grid is 40x40x40
    2. The parameters isomin and isomax basically define the colourscale. Values of outside of this range (ie: off the colourscale) don't get rendered at all
    3. The parameter surface_count defines how nice a rendering you get, but again a nicer rendering means longer loading time for the html file in Chrome.
    4. Griddata seems to be working well

--- Learning_dash/Griddata_test.py
+++ Learning_dash/Griddata_test.py
@@ -5,18 +5,41 @@ playing around to get a feel for the function
 
 import numpy as np
 from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 
 def func(x, y, z):
     return x+y+z
 
-x = np.linspace(0,1,100)
-y = np.linspace(0,1,100)
-z = np.linspace(0,1,100)
+x = np.linspace(0,1,40)
+y = np.linspace(0,1,40)
+z = np.linspace(0,1,40)
 
 grid_x, grid_y, grid_z = np.meshgrid(x,y,z)
 
 rng = np.random.default_rng()
-points = rng.random((1000, 3))
+points = rng.random((100, 3))
 values = func(points[:,0], points[:,1], points[:,2])
 
-grid = griddata(points, values, (grid_x, grid_y, grid_z), method='nearest')
+interp_values = griddata(points, values, (grid_x, grid_y, grid_z), method='nearest')
+
+
+# plotting data points in 3D graph
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(points[:,0], points[:,1], points[:,2])
+
+
+# plotting interpolated field in 3D graph
+fig = go.Figure(data=go.Volume(
+    x=grid_x.flatten(),
+    y=grid_y.flatten(),
+    z=grid_z.flatten(),
+    value=interp_values.flatten(),
+    isomin=0,
+    isomax=3,
+    opacity=0.1, # needs to be small to see through all surfaces
+    surface_count=17, # needs to be a large number for good volume rendering
+    ))
+
+fig.write_html("volume.html")
