f46c238494bdff2f01516618f62d3b8d2396b91f
Isaac Blanc <ib3418@ic.ac.uk>
2022-02-10 19:02:04 +0000

    Rbf maybe working - but losing shape

--- Learning_dash/Calculations.py
+++ Learning_dash/Calculations.py
@@ -7,7 +7,7 @@ liquid volume fraction, state of charge etc...
 
 # IMPORTING LIBRARIES
 import numpy as np
-from scipy.interpolate import griddata
+from scipy.interpolate import Rbf
 import plotly.graph_objects as go
 
 
@@ -42,7 +42,13 @@ def domain_interp(domain, sensor_locs, readings):
     # sensor values
     sensor_vals = np.array(readings).transpose()
     # interpolation
-    interp_vals = griddata(sensor_locs, sensor_vals, domain, method='linear')
+    rbf = Rbf(sensor_locs[:,0],
+              sensor_locs[:,1],
+              sensor_locs[:,2],
+              sensor_vals)
+    interp_vals = rbf(domain[0].flatten(),
+                      domain[1].flatten(),
+                      domain[2].flatten())
     return interp_vals
 
 def SoC(interp_vals):
@@ -66,25 +72,25 @@ def SoC(interp_vals):
 result = domain_interp(domain, sensor_locs, readings)
 
 
-# PLOTTING VOLUME
-# interpolated field
-fig1 = go.Figure(data=go.Volume(
-    x=domain[0].flatten(),
-    y=domain[1].flatten(),
-    z=domain[2].flatten(),
-    value=result.flatten(),
-    isomin=0,
-    isomax=10,
-    opacity=0.1, # needs to be small to see through all surfaces
-    surface_count=17, # needs to be a large number for good volume rendering
-    ))
-# sensor locations
-fig1.add_trace(go.Scatter3d(x=sensor_locs[:,0], y=sensor_locs[:,1], z=sensor_locs[:,2], mode='markers', marker={'color':'green'}))
-# save the result
-fig1.write_html("volume.html")
+# # PLOTTING VOLUME
+# # interpolated field
+# fig1 = go.Figure(data=go.Volume(
+#     x=domain[0].flatten(),
+#     y=domain[1].flatten(),
+#     z=domain[2].flatten(),
+#     value=result.flatten(),
+#     isomin=0,
+#     isomax=10,
+#     opacity=0.1, # needs to be small to see through all surfaces
+#     surface_count=17, # needs to be a large number for good volume rendering
+#     ))
+# # sensor locations
+# fig1.add_trace(go.Scatter3d(x=sensor_locs[:,0], y=sensor_locs[:,1], z=sensor_locs[:,2], mode='markers', marker={'color':'green'}))
+# # save the result
+# fig1.write_html("volume.html")
 
 
-# PLOTTING PLANE
-z_slice = 30 # note that this refers to slice index, not to dimensional location
-fig2 = go.Figure(data=go.Heatmap(x=domain[0][0,:,0], y=domain[1][:,0,0], z=result[:,:,z_slice], zsmooth='best'))
-fig2.write_html("plane.html")
+# # PLOTTING PLANE
+# z_slice = 30 # note that this refers to slice index, not to dimensional location
+# fig2 = go.Figure(data=go.Heatmap(x=domain[0][0,:,0], y=domain[1][:,0,0], z=result[:,:,z_slice], zsmooth='best'))
+# fig2.write_html("plane.html")
