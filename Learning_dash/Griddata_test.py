1ad5122c453d3b37bf601d14f2d608af49223ffc
Isaac Blanc <ib3418@ic.ac.uk>
2022-02-10 12:48:10 +0000

    making 'Griddata_test' neat

--- Learning_dash/Griddata_test.py
+++ Learning_dash/Griddata_test.py
@@ -3,34 +3,39 @@ example from: https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpo
 playing around to get a feel for the function
 '''
 
+
+# IMPORTING LIBRARIES
 import numpy as np
 from scipy.interpolate import griddata
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
-def func(x, y, z):
-    return x+y+z
 
+# CREATING SCATTERED DATAPOINTS IN 40x40x40 GRID
 x = np.linspace(0,1,40)
 y = np.linspace(0,1,40)
 z = np.linspace(0,1,40)
-
 grid_x, grid_y, grid_z = np.meshgrid(x,y,z)
+
+def func(x, y, z):
+    return x+y+z
 
 rng = np.random.default_rng()
 points = rng.random((100, 3))
 values = func(points[:,0], points[:,1], points[:,2])
 
-interp_values = griddata(points, values, (grid_x, grid_y, grid_z), method='nearest')
 
-
-# plotting data points in 3D graph
+# PLOTTING SCATTERED DATAPOINTS
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 ax.scatter(points[:,0], points[:,1], points[:,2])
 
 
-# plotting interpolated field in 3D graph
+# INTERPOLATING VALUES ACCROSS ALL OF GRID
+interp_values = griddata(points, values, (grid_x, grid_y, grid_z), method='nearest')
+
+
+# PLOTTING INTERPOLATED FIELD
 fig = go.Figure(data=go.Volume(
     x=grid_x.flatten(),
     y=grid_y.flatten(),
@@ -41,5 +46,4 @@ fig = go.Figure(data=go.Volume(
     opacity=0.1, # needs to be small to see through all surfaces
     surface_count=17, # needs to be a large number for good volume rendering
     ))
-
 fig.write_html("volume.html")
